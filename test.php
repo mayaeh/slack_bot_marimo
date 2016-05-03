915ae0e980b9ca5ae45feb2b7cf61b7113ecb39f
Maya Minatsuki <mayaeh@marimo-net.org>
2016-05-03 15:06:04 +0900

    CatchAll コマンドを追加し全てのイベントのデータを取得・対応できるよう変更

--- test.php
+++ test.php
@@ -6,8 +6,38 @@ use PhpSlackBot\Bot ;
 if ( ! defined ('SLACK_API_TOKEN') )
 { exit ("SLACK_API_TOKEN is not set.\n") ; }
 
+
+
+
+// This special command executes on all events
+class SuperCommand extends \PhpSlackBot\Command\BaseCommand {
+
+	protected function configure() {
+		// We don't have to configure a command name in this case
+	}
+
+	protected function execute($data, $context) {
+		if ($data['type'] == 'message') {
+			$channel = $this->getChannelNameFromChannelId($data['channel']);
+			$username = $this->getUserNameFromUserId($data['user']);
+            //echo $username.' from '.($channel ? $channel : 'DIRECT MESSAGE').' : '.$data['text'].PHP_EOL;
+
+
+// for debug
+$thismessage = $username . ' from ' . ($channel ? $channel : 'DM' ) . ' : ' . $data['text'] ;
+$this -> send($data['channel'], $data['user'], $thismessage);
+  
+  
+        }
+    }
+
+}
+
+
 $bot = new Bot () ;
 $bot -> setToken (SLACK_API_TOKEN) ;
+
+$bot->loadCatchAllCommand(new SuperCommand());
 
 $bot -> run () ;
 

7af0d4848a97781a218a961193d475b5c45550d7
Maya Minatsuki <mayaeh@marimo-net.org>
2016-05-09 22:37:16 +0900

    SQLite3 DB や Composer に関する初期設定方法を記述したファイルを追加 DB 構築に必要なファイルを追加

---
+++ setup/create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE word_verb (
+word TEXT UNIQUE, action TEXT
+);
+CREATE TABLE word_purpose (
+word TEXT UNIQUE, purpose TEXT
+);
+CREATE TABLE word_when (
+word TEXT UNIQUE, margin INTEGER
+);
+CREATE TABLE word_pref (
+word TEXT UNIQUE, id INTEGER
+);
+

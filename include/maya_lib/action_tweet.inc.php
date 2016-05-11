50e6d1e1eae735916f5712813b65ce69c2014d7b
Maya Minatsuki <mayaeh@marimo-net.org>
2016-05-11 18:45:52 +0900

    tweet 投稿処理を追加

--- include/maya_lib/action_tweet.inc.php
+++ include/maya_lib/action_tweet.inc.php
@@ -2,19 +2,62 @@
 
 function action_tweet ($text) {
 
-	if (isset($text)) {
+	if (!isset($text)) {
 		return null;
 	}
 
-	$connection = new TwitterOAuth 
-		(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, 
-		TWITTER_OAUTH_ACCESS_TOKEN, TWITTER_OAUTH_ACCESS_TOKEN_SECRET);
-
-	$content = $connection -> get 
-		("statuses/user_timeline", ["count" => 10, "page" => 1]);
+	// このやり方だけでは URL が含まれていた場合に正しく認識できない
+	// ( 公式短縮 URL のため )
+	if (mb_strlen($text) and mb_strlen($text) < 140) {
 
 // for debug
-var_dump($connection);
+//return mb_internal_encoding();
+//return mb_strlen($text);
+//$media_tmp_path = SCRIPT_DIR. 'resources/usagi_re_r.jpg';
+
+		$connection = new Abraham\TwitterOAuth\TwitterOAuth 
+			(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, 
+			TWITTER_OAUTH_ACCESS_TOKEN, TWITTER_OAUTH_ACCESS_TOKEN_SECRET);
+
+//	$content = $connection -> get 
+//		("statuses/user_timeline", ["count" => 1, "page" => 1]);
+
+
+
+		if(!isset($media_tmp_path)) {
+		
+			$parameters = ['status' => $text];
+		}
+		else {
+
+			$media = $connection ->
+				upload ('media/upload', 
+				['media' => $media_tmp_path]);
+
+			$parameters = ['status' => $text,
+				'media_ids' => $media -> media_id_string];
+		}
+
+		$statuses = $connection -> post 
+			('statuses/update', $parameters);
+
+
+// for debug
+//var_dump($connection);
+
+		$screen_name = $statuses -> user -> screen_name;
+
+		$id = $statuses -> id_str;
+
+		if (isset($screen_name) and isset($id)) {
+
+			$tweet_url = 'https://twitter.com/'. 
+				$screen_name . '/status/'. 
+				$id;
+
+			return $tweet_url;
+		}
+	}
 
 }
 
